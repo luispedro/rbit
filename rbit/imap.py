ec7b5f3b29f3e223274564eb27a6427bfdfaadf3
Luis Pedro Coelho <luis@luispedro.org>
2012-01-20 13:16:41 +0000

    ENH imapclient wrapper

---
+++ rbit/imap.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2012 Luis Pedro Coelho <luis@luispedro.org>
+# This file is part of rbit mail.
+
+import imapclient
+
+class IMAPClient(object):
+    def __init__(self, host, username, password):
+        self.connection = imapclient.IMAPClient(host, ssl=True)
+        self.connection.login(username, password)
+        self.folder = None
+
+    def _select_folder(self, folder):
+        if self.folder != folder:
+            self.connection.select_folder(folder)
+
+    def list_messages(self, folder):
+        self._select_folder(folder)
+        return self.search()
+
+    def retrieve(self, folder, message):
+        self._select_folder(folder)
+        return self.fetch(message, ['RFC822'])
+
+
