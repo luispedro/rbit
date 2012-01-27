cd4c26fffd273e6fe6073932b3e8f9a8d3efd446
Luis Pedro Coelho <luis@luispedro.org>
2012-01-27 17:08:14 +0000

    BLD setup.py for installation

---
+++ setup.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2012 Luis Pedro Coelho <luis@luispedro.org>
+# This file is part of rbit mail.
+try:
+    import setuptools
+except:
+    print '''
+setuptools not found.
+
+On linux, the package is often called python-setuptools'''
+    from sys import exit
+    exit(1)
+import os
+
+execfile('rbit/rbit_version.py')
+long_description = file('README.rst').read()
+
+undef_macros=[]
+if os.environ.get('DEBUG'):
+    undef_macros=['NDEBUG']
+
+extensions = {
+}
+
+ext_modules = []
+
+packages = setuptools.find_packages()
+
+classifiers = [
+]
+
+setuptools.setup(name='rbit',
+      version=__version__,
+      description='Rbit: A New Type of Mail Client',
+      long_description=long_description,
+      author='Luis Pedro Coelho',
+      author_email='luis@luispedro.org',
+      license='Proprietary',
+      platforms=['Any'],
+      classifiers=classifiers,
+      url='http://luispedro.org/software/rbit',
+      packages=packages,
+      ext_modules=ext_modules,
+      test_suite='nose.collector',
+      )
+
