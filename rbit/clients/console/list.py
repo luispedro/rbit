aa1b6e7b6bd261160d0eeb462b108233dababada
Luis Pedro Coelho <luis@luispedro.org>
2012-01-29 00:53:23 +0000

    RFCT Make list a function

--- rbit/clients/console/list.py
+++ rbit/clients/console/list.py
@@ -16,33 +16,37 @@ backend.init()
 session = backend.create_session()
 q = session.query(models.Message).all()
 
-text_header = 'Messages in INBOX'
-blank = urwid.Divider('=', bottom=1)
-listbox_content = []
-for message in q:
-    first_words = " ".join(message.body.split()[:32]) + "..."
-    text = urwid.Text([
-        ('bold_underline', message.from_),
-        " ",
-        ('bold', message.subject or "<no subject>"),
-        " ",
-        first_words])
-    listbox_content.extend([text, blank])
+def list_messages(folder):
+    text_header = 'Messages in %s' % folder
+    blank = urwid.Divider('=', bottom=1)
+    listbox_content = []
+    for message in q:
+        first_words = " ".join(message.body.split()[:32]) + "..."
+        text = urwid.Text([
+            ('bold_underline', message.from_),
+            " ",
+            ('bold', message.subject or "<no subject>"),
+            " ",
+            first_words])
+        listbox_content.extend([text, blank])
 
-header = urwid.AttrWrap(urwid.Text(text_header), 'header')
-listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
-frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
-palette = [
-        ('body','black','light gray', 'standout'),
-        ('header','white','dark red', 'bold'),
-        ('bold','black,bold','light gray', 'bold'),
-        ('bold_underline','black,bold,underline','light gray', 'bold,underline'),
-        ]
-screen = urwid.raw_display.Screen()
+    header = urwid.AttrWrap(urwid.Text(text_header), 'header')
+    listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
+    frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
+    return frame
 
 def unhandled(key):
     if key in ('q','Q'):
         raise urwid.ExitMainLoop()
+
+palette = [
+        ('body','black','white', 'standout'),
+        ('header','white','dark red', 'bold'),
+        ('bold','black,bold','white', 'bold'),
+        ('bold_underline','black,bold,underline','white', 'bold,underline'),
+        ]
+frame = list_messages('INBOX')
+screen = urwid.raw_display.Screen()
 
 urwid.MainLoop(frame, palette, screen, unhandled_input=unhandled).run()
 
