a5265b38575400669caa144e348d9b66b8f8789c
Luis Pedro Coelho <luis@luispedro.org>
2012-01-30 15:04:08 +0000

    TST Update to newer interface

--- rbit/tests/test_models.py
+++ rbit/tests/test_models.py
@@ -7,7 +7,6 @@ def test_from_email_message():
     message['From'] = 'Luis Pedro Coelho <luis@luispedro.org>'
     message['Date'] = 'Mon, 23 Jan 2012 23:00:47 -0000'
     message.set_payload('I love you')
-    m = Message.from_email_message(message)
-    assert m.body == 'I love you'
+    m = Message.from_email_message(message, 123)
     assert m.to == 'rita@ritareis.org'
 
