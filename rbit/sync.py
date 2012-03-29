7adf54cf99b793e65cd55dcadd5a1459a447c16a
Luis Pedro Coelho <luis@luispedro.org>
2012-03-29 18:39:31 +0100

    BUG fix variable name screwup

--- rbit/sync.py
+++ rbit/sync.py
@@ -195,11 +195,11 @@ def update_folder(client, folder, create_session=None):
         changed = client.fetch_flags_since(prevmodseq)
         for uid in changed:
             m = load_message(folder, uid, lambda: session)
-            flags = set(changed[uid]['FLAGS'])
+            rfs = set(changed[uid]['FLAGS'])
+            lfs = set(f.flag for f in m.flags)
             for f in m.flags:
                 if f.flag not in rfs:
                     session.delete(f)
-            lfs = set(f.flag for f in m.flags)
             for f in rfs-lfs:
                 session.add(models.Flag(mid=m.mid, flag=f))
         session.commit()
