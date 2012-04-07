b28759ea236415f2b12d753d1004194fa4b76a09
Luis Pedro Coelho <luis@luispedro.org>
2012-04-07 14:03:15 +0100

    DOC Document train_on_all()

--- rbit/ml/train.py
+++ rbit/ml/train.py
@@ -7,7 +7,20 @@ from rbit.backend import call_create_session
 
 from vw_learner import VWLearner
 
-def train_model(create_session=None):
+def train_on_all(create_session=None):
+    '''
+    model = train_on_all(create_session={backend.create_session})
+
+    Train a model on all messages
+
+    Parameters
+    ----------
+    create_session : callable, optional
+
+    Returns
+    -------
+    model : A Model
+    '''
     session = call_create_session(create_session)
     ms = session.query(models.Message.folder, models.Message.mid).all()
     labels = [m.folder for m in ms]
@@ -15,7 +28,8 @@ def train_model(create_session=None):
     mids = [m.mid for m in ms]
     return learner.train(mids, labels)
 
+
 if __name__ == '__main__':
     import pickle
-    model = train_model()
+    model = train_on_all()
     pickle.dump(model, file('model.pp','w'))
