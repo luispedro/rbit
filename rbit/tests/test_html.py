ae2ef35874454b350623779fa3de3a0f8f50dfbb
Luis Pedro Coelho <luis@luispedro.org>
2012-07-26 12:02:15 +0100

    TST Fix test path

--- rbit/tests/test_html.py
+++ rbit/tests/test_html.py
@@ -1,4 +1,4 @@
-from html import text_from_html
+from rbit.html import text_from_html
 
 def test_text_from_html():
     assert text_from_html('<html><body><p>This is the text</p></body></html>') == "This is the text"
