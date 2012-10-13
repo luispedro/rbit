1a8f5078a25e8298da69a4ec2c830f94268d3ff8
Luis Pedro Coelho <luis@luispedro.org>
2012-10-13 12:13:10 +0100

    GUI Histogram of timestamps

---
+++ gui/MatplotlibWidget.py
@@ -0,0 +1,23 @@
+import matplotlib
+import numpy as np
+
+
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4']='PySide'
+
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+
+
+class MatplotlibWidget(FigureCanvas):
+
+    def __init__(self, parent=None):
+        super(MatplotlibWidget, self).__init__(Figure())
+
+        self.setParent(parent)
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.axes = self.figure.add_subplot(1,1,1)
+
+
--- gui/graphs.py
+++ gui/graphs.py
@@ -4,6 +4,41 @@ from os import path
 from PySide import QtCore, QtGui, QtUiTools
 from PySidePlus import qopen
 
+def calc_grid():
+    from rbit import models
+    from rbit.backend import create_session
+    import numpy as np
+    session = create_session()
+    grid = np.zeros((7,24))
+    for d in session.query(models.Message.date):
+        (d,) = d
+        grid[d.weekday(), d.hour] += 1
+    grid /= grid.sum()
+    return grid
+
+def plot_hours(dialog, grid):
+    import numpy as np
+    from MatplotlibWidget import MatplotlibWidget
+    hours = MatplotlibWidget(dialog)
+
+    hours.axes.bar(np.arange(24), grid.sum(0)*100)
+    hours.axes.set_ylabel('Fraction of messages (%)')
+    hours.axes.set_xlabel('hour of the day')
+    hours.canvas.show()
+    return hours
+
+def plot_days(dialog, grid):
+    import numpy as np
+    from MatplotlibWidget import MatplotlibWidget
+    days = MatplotlibWidget(dialog)
+
+    days.axes.bar(np.arange(7), grid.sum(1)*100)
+    days.axes.set_ylabel('Fraction of messages (%)')
+    days.axes.set_xlabel('Day of the Week')
+    days.canvas.show()
+    return days
+
+
 
 # We need to save a reference to the dialog object
 # Otherwise, it gets garbage collected.
@@ -18,4 +53,10 @@ def show_graph_dialog():
     with qopen(uifilepath) as uifile:
         dialog = loader.load(uifile)
 
+    dialog.tabWidget.clear()
+    # This should be off-loaded to a background thread
+    grid = calc_grid()
+    dialog.tabWidget.addTab(plot_hours(dialog, grid), u'Hours')
+    dialog.tabWidget.addTab(plot_days(dialog, grid), u'Days')
     dialog.show()
+
